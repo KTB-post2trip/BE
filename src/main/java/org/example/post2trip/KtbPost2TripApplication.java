afc19ee13d6cf76a3e63b1b59ca83ec13d0f49be
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2025-02-25 20:18:27 +0900

    초기 프로젝트 생성

---
+++ src/main/java/org/example/post2trip/KtbPost2TripApplication.java
@@ -0,0 +1,13 @@
+package org.example.post2trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbPost2TripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbPost2TripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/post2trip/KtbPost2TripApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.post2trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbPost2TripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
