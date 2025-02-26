c791923483ce9be2d084de274630d12ff17eed23
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2025-02-26 14:24:07 +0900

    swagger 설정

---
+++ src/main/java/org/example/post2trip/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package org.example.post2trip.global.config;
+
+import java.util.ArrayList;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        servers = {
+                //	@Server(url = "${swagger.url}", description = "Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Server")
+        },
+        info = @Info(
+                title = "post2Trip API 명세서",
+                description = "post2Trip API 명세서",
+                version = "v1"
+        )
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+
+
+
+
+
+
+}
