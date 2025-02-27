e96ad2ad3aacdcca8abe892f89717a02e694e763
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2025-02-28 01:32:30 +0900

    cors 설정

--- src/main/java/org/example/post2trip/global/config/CorsConfig.java
+++ src/main/java/org/example/post2trip/global/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://post2trip-fe.vercel.app/")); // 🔹 허용할 도메인
+        config.setAllowedOrigins(List.of("*")); // 🔹 허용할 도메인
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 🔹 허용할 HTTP 메서드
         config.setAllowedHeaders(List.of("*")); // 🔹 모든 헤더 허용
         config.setAllowCredentials(true); // 🔹 인증 정보 포함 허용 (JWT 사용 시 필요)
